f41f3a032946a0a016e51216eae72b6b38f2992c
welly80 <61974391+welly80@users.noreply.github.com>
2020-03-09 13:00:04 +0000

    main file

---
+++ TamagotchiWorld.java
@@ -0,0 +1,12 @@
+package CW2;
+import java.util.*;
+
+public class TamagotchiWorld {
+	
+	//--main method needed in all programs
+	public static void main(String[] args) {
+		Tamagotchi buggie = new Tamagotchi();
+		buggie.mainLoop();
+	}
+
+}
