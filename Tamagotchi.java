39b984db4dd2d4c660188354ebebe70c0587cd60
Richard Mills <61974792+rmills-52@users.noreply.github.com>
2020-03-11 14:22:06 +0000

    Update Tamagotchi.java

--- Tamagotchi.java
+++ Tamagotchi.java
@@ -11,10 +11,6 @@ public class Tamagotchi {
  static Scanner console = new Scanner(System.in); //Declare console for input
 
  
-
-
-
-
 /* Varaibles controlling the tamagotchi's health */
 
 static double happiness = 100;
@@ -31,6 +27,7 @@ static String done = "done";
 
 /* End of tamagotchi health */
 
+>>>>>>> master
  
 
  private String state;
@@ -81,8 +78,6 @@ static String done = "done";
 
 	  System.out.println("The tamagotchi is happy and looking at you.");
 
-	  
-
 	  System.out.println("What would you like to do?");
 
 	  System.out.println(" 1) Walk the Tamagotchi.");
@@ -91,6 +86,7 @@ static String done = "done";
 
 	  System.out.println(" 3) Quite program.");
 
+>>>>>>> master
 	  action = console.nextInt();
 
 	  
@@ -119,7 +115,26 @@ static String done = "done";
 
   } 
 
+   private void hungryMenu() {
+	   int action;
+	   System.out.println("The tamagotchi is now beginning to start feeling hungry and needs feeding.");
 	
+           System.out.println("What would you like to do?");
+	   System.out.println(" 1) Feed the Tamagotchi pet food/water.");
+	   System.out.println(" 2) Let the Tamagotchi starve.");
+	   System.out.println(" 3) Quit program.");
+	   
+	   if (action ==1) {
+		   System.out.println("The Tamagotchi is now feeling happy being fed pet food/water".)
+		   state = happy;
+	   } else if (action == 2) {
+		   System.out.println("The Tamagotchi is now feeling undernourished after not being fed pet food/water".)
+	           state = sick;
+	   } else if(action =- 3) {
+		   state = done;
+	   } //end of else if statement (hungry) state for Tamagotchi
+   }
+		
 
    private void tiredMenu() {
 
@@ -225,7 +240,10 @@ static String done = "done";
 				  age =+ 1;
 
 				  break;
-
+			  
+				  case hungry:
+					hungryMenu();
+					break;
 
 			  case "tired":
 
