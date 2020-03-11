52212ef1f1a5f61ac0856406b009d985e3a7d82e
Joshua-Rex <61975049+Joshua-Rex@users.noreply.github.com>
2020-03-11 14:26:43 +0000

    Update TamagotchiWorld.java

--- TamagotchiWorld.java
+++ TamagotchiWorld.java
@@ -5,6 +5,9 @@ public class TamagotchiWorld {
 	
 	//--main method needed in all programs
 	public static void main(String[] args) {
+		String name;
+		System.out.println("What name do you want for your Tamagotchi?")
+		name = 
 		Tamagotchi buggie = new Tamagotchi();
 		buggie.mainLoop();
 	}
